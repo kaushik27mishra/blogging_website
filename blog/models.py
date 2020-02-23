0dab9adf6adc5be9004b983c0c1efdeadd52a50b
Kaushik Mishra <kmishra27.km@gmail.com>
2020-02-23 12:10:49 +0530

    learned making migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone as tz
+from django.contrib.auth.models import User
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=100)
+    date_posted = models.DateTimeField(default=tz.now)
+    #as posts are unique to a author we use Foreign Keys
+    author = models.ForeignKey(User, on_delete= models.CASCADE) 
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author':'CoreyMS',
+        'title':'Blog Post 1',
+        'content':'First Post content',
+        'date_posted':'February 23, 2020'
+    },
+    {
+        'author':'Jane doe',
+        'title':'Blog Post 2',
+        'content':'Second Post content',
+        'date_posted':'February 24, 2020'
+    }
+]
+
 def home(request):
-    return render(request,'blog/home.html')  
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)  
 
 # Create your views here.
 
 def about(request):
-    return render(request,'blog/about.html')  
+    return render(request,'blog/about.html',{'title':'About'})  
